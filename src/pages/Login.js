689fcde44c0a878e793bde7bcea2af49564dad92
Naimuddin <naimuddin.saifi@innovationm.com>
2021-11-23 18:23:09 +0530

    login added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,7 @@ function Login() {
 
     const handleClick = () => {
         // if (localStorage.getItem('token')) {
-            const client_id = 'client_id123'
+            const client_id = 'clientid123'
             const redirect_uri = 'http://localhost:3000'
             const auth_server_frontend="https://secure-river-45347.herokuapp.com"
             const state = 'state'
