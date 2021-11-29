8cd4dd7c13e2232e3005a66bbb0a2f6b33e3c20c
Naimuddin <naimuddin.saifi@innovationm.com>
2021-11-29 13:34:28 +0530

    loclastorage floew

--- src/pages/Code.js
+++ src/pages/Code.js
@@ -3,18 +3,20 @@ import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom"
 
 
-function Code(props) {  
+function Code(props) {
     const location = useLocation()
     const navigate = useNavigate()
 
     useEffect(() => {
         const code = new URLSearchParams(location && location.search).get("code")
         const redirect_uri = process.env.REACT_APP_REDIRECT_URI || 'http://localhost:3000'
-        const url = `${process.env.REACT_APP_SERVER_URL}/api/oauth/token?code=${code}&grant_type=authorization_code&redirect_uri=${redirect_uri}&client_id=cid&client_secret=csec`
+        const client_id = process.env.REACT_APP_CLIENT_ID || 'uidWomMlTsJhRam0zm8AK'
+        const client_secret = process.env.REACT_APP_CLIENT_SECRET || '0nh-UdU_Vi4Geo73Ek78-'
+        const url = `${process.env.REACT_APP_SERVER_URL}/api/oauth/token?code=${code}&grant_type=authorization_code&redirect_uri=${redirect_uri}&client_id=${client_id}&client_secret=${client_secret}`
         axios.get(url)
             .then(res => {
                 console.log(res)
-                localStorage.setItem('access_token', res && res.data && res.data.data.access_token)
+                // localStorage.setItem('access_token', res && res.data && res.data.data.access_token)
                 // navigate('/home')
             })
             .catch(err => {
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,9 +4,9 @@ function Login() {
     const navigate = useNavigate()
 
     const handleClick = () => {
-        const client_id = process.env.REACT_APP_CLIENT_ID || 'clientid123'
+        const client_id = process.env.REACT_APP_CLIENT_ID || 'E6qyTeX-cDiRB6IwwCBZR'
         const redirect_uri = process.env.REACT_APP_REDIRECT_API || 'http://localhost:3000'
-        const state = 'state123'
+        const state = 'state1234567890'
         const auth_frontend_url = process.env.REACT_APP_AUTH_FRONTEND_URL || "https://secure-river-45347.herokuapp.com"
         const url = `${auth_frontend_url}/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&state=${state}`
         window &&
